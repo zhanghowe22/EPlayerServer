ec54f65e353d8704e1891599c147453a6ae51213
zhanghowe <zhanghowe122@gmail.com>
2025-03-13 16:02:13 +0800

    main: 进程代码测试

--- EPlayerServer/main.cpp
+++ EPlayerServer/main.cpp
@@ -5,6 +5,8 @@
 #include <memory.h>
 #include <sys/socket.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 class CFunctionBase
 {
@@ -72,7 +74,8 @@ public:
         if (pid == 0) {
             close(pipes[1]); // 子进程关闭掉写
             pipes[1] = 0;
-            return (*m_func)();
+            ret = (*m_func)();
+            exit(0);
         }
 
         // 主进程
@@ -85,13 +88,14 @@ public:
     // 发送文件描述符
     int SendFD(int fd) { // 主进程完成
         struct msghdr msg {};
-        iovec iov[2];
-        iov[0].iov_base = (char*)"edoyun";
-        iov[0].iov_len = 7;
-		iov[1].iov_base = (char*)"jueding";
-		iov[1].iov_len = 8;
-        msg.msg_iov = iov;
-        msg.msg_iovlen = 2;
+		iovec iov[2];
+		char buf[2][10] = { "edoyun" , "jueding" };
+		iov[0].iov_base = buf[0];
+		iov[0].iov_len = sizeof(buf[0]);
+		iov[1].iov_base = buf[1];
+		iov[1].iov_len = sizeof(buf[1]);
+		msg.msg_iov = iov;
+		msg.msg_iovlen = 2;
 
         // 下面的数据才是我们需要传递的
         cmsghdr* cmsg = (cmsghdr*)calloc(1, CMSG_LEN(sizeof(int)));
@@ -140,6 +144,7 @@ public:
         }
 
         fd = *(int*)CMSG_DATA(cmsg);
+        free(cmsg);
         return 0;
     }
 
@@ -151,23 +156,52 @@ private:
 
 int CreateLogServer(CProcess* proc)
 {
+    printf("%s(%d):<%s> pid = %d\n", __FILE__, __LINE__, __FUNCTION__, getpid());
     return 0;
 }
 
 int CreateClientServer(CProcess* proc)
 {
+    printf("%s(%d):<%s> pid = %d\n", __FILE__, __LINE__, __FUNCTION__, getpid());
+	int fd = -1;
+	int ret = proc->RecvFD(fd);
+	printf("%s(%d):<%s> ret = %d\n", __FILE__, __LINE__, __FUNCTION__, ret);
+	printf("%s(%d):<%s> fd = %d\n", __FILE__, __LINE__, __FUNCTION__, fd);
+	sleep(1);
+	char buf[10] = "";
+    lseek(fd, 0, SEEK_SET);
+	read(fd, buf, sizeof(buf));
+	printf("%s(%d):<%s> buf is: %s\n", __FILE__, __LINE__, __FUNCTION__, buf);
+	close(fd);
     return 0;
 }
 
 int main()
 {
     CProcess proclog, procclients;
-
+    printf("%s(%d):<%s> pid = %d\n", __FILE__, __LINE__, __FUNCTION__, getpid());
     proclog.SetEntryFunction(CreateLogServer, &proclog);
     int ret = proclog.CreateSubProcess();
-    if (ret != 0) return -1;
+    if (ret != 0) {
+        printf("%s(%d):<%s> pid = %d\n", __FILE__, __LINE__, __FUNCTION__, getpid());
+        return -1;
+    }
+    printf("%s(%d):<%s> pid = %d\n", __FILE__, __LINE__, __FUNCTION__, getpid());
     procclients.SetEntryFunction(CreateClientServer, &procclients);
     ret = procclients.CreateSubProcess();
-    if (ret != 0) return -2;
+    if (ret != 0) {
+        printf("%s(%d):<%s> pid = %d\n", __FILE__, __LINE__, __FUNCTION__, getpid());
+        return -2;
+    }
+    printf("%s(%d):<%s> pid = %d\n", __FILE__, __LINE__, __FUNCTION__, getpid());
+    //usleep(100 * 000);
+	int fd = open("./test.txt", O_RDWR | O_CREAT | O_APPEND);
+	printf("%s(%d):<%s> fd = %d\n", __FILE__, __LINE__, __FUNCTION__, fd);
+	if (fd == -1) return -3;
+	ret = procclients.SendFD(fd);
+	printf("%s(%d):<%s> ret = %d\n", __FILE__, __LINE__, __FUNCTION__, ret);
+	write(fd, "edoyun", 6);
+	close(fd);
+
     return 0;
 }
